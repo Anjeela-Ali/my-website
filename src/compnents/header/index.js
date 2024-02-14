d606ce7c4c69e3c7ade5a137587d0066d8e21edc
anjeelaali <anjeelaali6@gmail.con>
2024-02-14 17:15:10 +0500

    Header is added .

--- src/compnents/header/index.js
+++ src/compnents/header/index.js
@@ -4,7 +4,21 @@ const Header = () => {
   return (
     <>
 
-    <div className='border p-3 bg-[#000214]'>
+    <div className=' p-3 bg-[#000214] flex justify-between '>
+        {/* Logo */}
+        <div>
+            <h2 className='text-[#116A7B] px-[80px] text-2xl font-bold text' >
+                Logo
+            </h2>
+        </div>
+        {/* Sign up div */}
+        <div>
+            <input className='bg-inherit border rounded-full p-2 px-6 ' placeholder='Email' type='email'/>
+            <button className=' border p-2 px-12 mx-2 bg-white font-bold text-[#000214] rounded-full '>
+                Sign Up
+            </button>
+
+        </div>
 
     </div>
       
